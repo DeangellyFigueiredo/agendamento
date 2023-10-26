7a69b0c2f23038a4a8411b1acd8e98c71da4170c
Deangelly Figueiredo <deangelly.figueiredo@grupoicts.com.br>
2023-10-26 16:56:11 -0400

    feat:card options

---
+++ src/app/options/options.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-options',
+  templateUrl: './options.component.html',
+  styleUrls: ['./options.component.css'],
+})
+export class OptionsComponent {
+  display = false;
+  status = 'Livre';
+  currentTime = '11:00';
+  currentDate = '11 de outubro de 2023';
+  nextSchedule = '11 de outubro de 2023 | 14:00';
+}

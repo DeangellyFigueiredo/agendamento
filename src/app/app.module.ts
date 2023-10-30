c5829ee92f4bc27e7068bbc7c14c7f55caa0ed84
Deangelly Figueiredo <deangelly.figueiredo@grupoicts.com.br>
2023-10-30 08:47:16 -0400

    fix: adjusting css inline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TimelineComponent } from './timeline/timeline.component';
 import { OptionsComponent } from './options/options.component';
 import { CardComponent } from './card/card.component';
+import { DisplayService } from './display.service';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +17,10 @@ import { CardComponent } from './card/card.component';
     TimelineComponent,
     OptionsComponent,
     CardComponent,
+    ScheduleComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [DisplayService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DisplayService } from '../display.service';
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.css'],
 })
-export class OptionsComponent {
-  display = false;
+export class OptionsComponent implements OnInit {
   status = 'LIVRE';
   currentTime = '11:00';
   currentDate = '11 de outubro de 2023';
   nextSchedule = '11 de outubro de 2023 | 14:00';
+  display = false;
+  onClick() {
+    this.displayService.displayChange.emit(false);
+  }
+
+  constructor(private displayService: DisplayService) {
+    this.displayService.displayChange.subscribe((value) => {
+      this.display = value;
+    });
+  }
+  ngOnInit(): void {
+    this.display = this.displayService.display;
+  }
 }
